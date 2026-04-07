f11733a3401ba0082aa70457fcb1883d77abd4c4
EsruDXT <amengceria2017@gmail.com>
2026-04-07 07:28:03 +0700

    feat: implement submit button student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -20,8 +20,8 @@
             <p>Menampilkan form tambah siswa.</p>
         </div>
 <!-- Card Content -->
-        <div class="container mx-auto p-4 mt-4 bg-white shadow rounded-lg items-center">
-            <form action="/students" method="POST" class="p-4 grid grid-cols-2 gap-4" >
+        <div class="container mx-auto p-4 mt-4 bg-white shadow rounded-lg">
+            <form action="/students" method="POST" class="p-4 grid grid-cols-2 gap-4">
                 <div class="mb-4">
                     <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nama:</label>
                     <input type="text" id="name" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Masukkan Nama">
@@ -38,9 +38,14 @@
                     <label for="phone" class="block text-gray-700 text-sm font-bold mb-2">No Telepon:</label>
                     <input type="text" id="phone" name="phone" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Masukkan No Telepon">
                 </div>
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                <div class="flex justify-end gap-4 col-span-2">
+                <a href="/students" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-[10%] flex items-center justify-center">
+                    Kembali
+                </a>
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-[10%] flex items-center justify-center ">
                     Tambah Siswa
                 </button>
+                </div>
             </form>
         </div>
     </main>
