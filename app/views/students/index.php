a3d68ff090ffdb5185c7dae8771f9c6148e2dca3
EsruDXT <amengceria2017@gmail.com>
2026-04-17 11:09:16 +0700

    feat: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -1,5 +1,5 @@
     <?php require_once '../app/views/layouts/partials/header.php'; ?>
-    <main class="bg-gray-100 grow flex-grow">
+    <main class="bg-gray-100 grow py-6">
         <div class="bg-white shadow rounded-lg container mx-auto p-4 mt-4">
             <h1 class="text-2xl font-bold mb-4">Daftar Siswa</h1>
             <p>Menampilkan daftar siswa yang terdaftar.</p>
@@ -17,22 +17,26 @@
                         <th class="py-2 px-4">Aksi</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody> 
+                        <?php foreach ($data['students'] as $index => $student) : ?>
+                                                        <tr>
+                                <td class="py-2 px-4 text-left"><?= $index + 1 ?></td>
+                                <td class="py-2 px-4 text-left"><?= $student['name'] ?></td>
+                                <td class="py-2 px-4 text-left"><?= $student['nis'] ?></td>
+                                <td class="py-2 px-4 text-left"><?= $student['class'] ?></td>
+                                <td class="py-2 px-4 text-left"><?= $student['phone_number'] ?></td>
+                                <td class="py-2 px-4 text-center">
+                                    <div class="flex items-center justify-center gap-4">
+                                        <a href="/students/<?= $student['id'] ?>" class="text-green-500 hover:underline">Detail</a>
+                                        <a href="/students/<?= $student['id'] ?>/edit" class="text-yellow-500 hover:underline">Edit</a>
+                                        <a href="" class="text-red-500 hover:underline">Hapus</a>
+                    
                     <!-- Contoh data siswa -->
-                    <tr>
-                        <td class="py-2 px-4 text-left">1</td>
-                        <td class="py-2 px-4 text-left">Harry Wang</td>
-                        <td class="py-2 px-4 text-left">999</td>
-                        <td class="py-2 px-4 text-left">XI TKJ 3</td>
-                        <td class="py-2 px-4 text-left">081352689235</td>
-                        <td class="py-2 px-4 text-center">
-                            <div class="flex items-center justify-center gap-4">
-                                <a href="/students/1" class="text-green-500 hover:underline">Detail</a>
-                                <a href="/students/1/edit" class="text-yellow-500 hover:underline">Edit</a>
-                                <a href="" class="text-red-500 hover:underline">Hapus</a>
                             </div>
                         </td>
                     </tr>
+                        <?php endforeach; ?>
+
                 </tbody>
             </table>
         </div>
